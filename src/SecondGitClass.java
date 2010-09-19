a3b69f93390fb66c123d5b0dfbe07202e5f3cbce
mona <m_mehran@cs.concordia.ca>
2010-09-19 12:30:23 -0400

    improvving implementations

--- src/SecondGitClass.java
+++ src/SecondGitClass.java
@@ -3,7 +3,7 @@ public class SecondGitClass {
 
 	public static void main (String[] args)
 		{
-			System.out.println("Testing git for the second time ");
+			System.out.println("Improving implementation ");
 		}
 	
 }
