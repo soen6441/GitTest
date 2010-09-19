ae8f5ec7b4d1893be0ca4ebef7d849662669f5f3
mona <m_mehran@cs.concordia.ca>
2010-09-19 12:26:46 -0400

    testing git for second time
    Signed-off-by: mona <m_mehran@cs.concordia.ca>

--- src/GitClass.java
+++ src/GitClass.java
@@ -2,6 +2,6 @@
 public class GitClass {
 	public static void main (String[] args)
 	{
-		System.out.println("Testing git for the second time ");
+		System.out.println("Testing git ");
 	}
 }
---
+++ src/SecondGitClass.java
@@ -0,0 +1,9 @@
+
+public class SecondGitClass {
+
+	public static void main (String[] args)
+		{
+			System.out.println("Testing git for the second time ");
+		}
+	
+}
